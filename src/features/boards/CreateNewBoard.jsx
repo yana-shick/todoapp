c2f59b4871e6215dda4fc7f3416848ce082b04f8
yana shick <shickya@gmail.com>
2024-09-19 21:14:33 +0300

    style

--- src/features/boards/CreateNewBoard.jsx
+++ src/features/boards/CreateNewBoard.jsx
@@ -1,5 +1,16 @@
 import { useCreateBoard } from "./useCreateBoard";
 
+import styled from "styled-components";
+
+import AppInput from "../../ui/AppInput";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
 export default function CreateNewBoard() {
   const { createBoard } = useCreateBoard();
 
@@ -15,8 +26,29 @@ export default function CreateNewBoard() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input placeholder="create new board" />
-        <button type="submit">Create</button>
+        <Container>
+          <AppInput
+            label="*"
+            name="board"
+            type="text"
+            placeholder="Name board..."
+          />
+
+          <button
+            style={{
+              width: "100px",
+              height: "30px",
+              fontSize: "12px",
+              background: "none",
+              border: "1px solid whitesmoke",
+              color: "whitesmoke",
+              cursor: "pointer",
+            }}
+            type="submit"
+          >
+            Create
+          </button>
+        </Container>
       </form>
     </div>
   );
